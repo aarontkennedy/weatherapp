f3c0ab1423acfcec1e5c90faf78b48229f6bdb75
Kennedy <aarontkennedy@gmail.com>
2020-02-12 21:25:17 -0600

    gets the weather api working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import callWeatherApi from './utils/weatherapi';
+import LocationSearch from './components/locationSearch'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "Minneapolis",
+      location: null,
+      current: null,
+    };
+    const API_KEY =`${process.env.REACT_APP_WEATHER_API_KEY}`
+    console.log(API_KEY);
+  }
+
+  componentDidMount() {
+    console.log(this.state);
+    callWeatherApi.getCurrent(this.state.query)
+            .then((data) => {
+                console.log(data.data);
+                this.setState({location: data.data.location});
+                this.setState({current: data.data.current});
+                console.log(this.state);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+            console.log(this.state);
+
+    // callWeatherApi.getHistory(this.state.query)
+    //         .then((data) => {
+    //             console.log(data.data);
+    //             this.setState({location: data.data.history});
+    //             console.log(this.state);
+    //         })
+    //         .catch((e) => {
+    //             console.log(e);
+    //         });
+  }
+
+  // componentWillUnmount() {
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <LocationSearch />
+
+          <p>{ this.state.location ? this.state.location.name : ""}</p>
+
+          <p>{ this.state.current ? this.state.current.temp_f : ""}</p>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
